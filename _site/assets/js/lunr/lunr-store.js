62660388beb080d07c8eed136a69a998cec5f15e
Patrick Crumley <patrick.crumley@gmail.com>
2020-08-23 11:38:26 -0400

    added twitter links and drafts

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,13 @@
 var store = [{
-        "title": "Hello, there",
-        "excerpt":"Hi. I’m Patrick. Right now I work as a computational astrophysicist, but I am also in the middle of a career transition. I am starting a blog both as a way to find ways to connect in this new normal, write down my thoughts, and document things about my work...","categories": ["Blog"],
+        "title": "Hello There.",
+        "excerpt":"Hi. I’m Patrick. Right now I work as a computational astrophysicist, but I am also in the middle of a career transition. I am starting a blog to find ways to connect in this new normal, write down my thoughts, and document things about my work in and outside of...","categories": ["Blog"],
         "tags": ["Personal","Voorwerpjes"],
         "url": "http://localhost:4000/blog/hello-there/",
+        "teaser": null
+      },{
+        "title": "Handy Resources for learning & doing ML",
+        "excerpt":"I attended the 2020 Spring session of Insight Data Science NYC, a 7 week long, postdoctoral professional development fellowship from January through March. I had a lot of fun doing it, but it was also surreal to watch the world shift under our feet in early March during the outbreak...","categories": ["Blog"],
+        "tags": ["Insight Data Science","NYC"],
+        "url": "http://localhost:4000/blog/stats-ml-resources/",
         "teaser": null
       }]
